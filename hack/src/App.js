d6d1e5f56fb24fcfdcfa4b93e021e63dd4b09be4
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-07-01 20:51:53 +0530

    Add files via upload

---
+++ hack/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './index.css'; // Import the corresponding CSS file
+
+const LoginForm = () => {
+  return (
+    <div className="box">
+      <div className="form">
+        <h2>Sign in</h2>
+        <div className="inputBox">
+          <input type="text" required="required" />
+          <span>Username</span>
+          <i></i>
+        </div>
+        <div className="inputBox">
+          <input type="password" required="required" />
+          <span>Password</span>
+          <i></i>
+        </div>
+        <div className="links">
+          <a href="#">Forgot Password</a>
+          <a href="#">Signup</a>
+        </div>
+        <input type="submit" value="Login" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
